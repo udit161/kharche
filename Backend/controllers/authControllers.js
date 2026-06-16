56109ccf4f66d7a68b001d8667ba0051b0e710ba
udit161 <udit161@users.noreply.github.com>
2026-06-16 22:40:58 +0530

    registerand createuser

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -6,9 +6,27 @@ const generateToken = (id) => {
 
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
-  const userExists = await User.findOne({
-    email,
-  });
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Please fill all fields" });
+  }
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 exports.LoginUser = async (req, res) => {};
