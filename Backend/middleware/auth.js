25c695d34688f7adfea95fa06798f6ee7dd6863c
udit161 <udit161@users.noreply.github.com>
2026-06-23 13:06:02 +0530

    authmiddleware

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,7 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
   if (!token) {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
